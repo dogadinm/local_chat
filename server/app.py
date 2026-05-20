06bca8b5702a82f6551c47d773f8cce99d604b92
doagdinm <dogadin01@gmail.com>
2026-05-20 21:01:19 +0200

    added middlewar

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from backend.adapters.registry import registry
 from server.routes import router
@@ -17,4 +18,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
